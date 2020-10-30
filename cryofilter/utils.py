1be0a127a412111f25779835f67c28488e335deb
smthomas-sci <simon.thomas@uq.edu.au>
2020-10-30 18:11:31 +1000

    added metrics to utils

--- cryofilter/utils.py
+++ cryofilter/utils.py
@@ -12,58 +12,100 @@ Requirements (available by pip / conda):
 """
 
 from sklearn.metrics import roc_curve, auc, roc_auc_score
+import os
 
-y_val = []
-X1_val = []
-X2_val = []
+import matplotlib.pyplot as plt
 
-val_gen.batch_size = 1
+import tensorflow as tf
 
-for i in range(val_gen.n):
-    X, y = val_gen[i]
-
-    y_val.append(y[0])
-    X1_val.append(X[0][0])
-    X2_val.append(X[1][0])
-
-y_val = np.stack(y_val)
-X1_val = np.stack(X1_val)
-X2_val = np.stack(X2_val)
+import numpy as np
 
 
-# generate a no skill prediction (majority class)
-ns_probs = [0 for _ in range(val_gen.n)]
+from cryofilter.data import DataGenerator
+from cryofilter.model import build_model
 
-# predict probabilities
+# # --------------------- DEBUG -------------------------------------- #
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+# # ----------------------------------------------------------------- #
 
-lr_probs = model.predict([X1_val, X2_val], verbose=1)
 
 
+# 1. Setup data
+generator = DataGenerator(pos_files=["../data/pos.mrcs", "../data/pos_top.mrcs"],
+                          neg_files=["../data/neg.mrcs"],
+                          batch_size=32,
+                          split=0.7,
+                          img_dim=28
+                          )
+
+model = build_model(28)
+
+model.load_weights("../weights/weights_epoch_99_val_acc_0.981.h5")
+
+model.summary()
+
+y_preds = []
+y_trues = []
+
+N = generator.val.n // generator.batch_size
+
+counts = 0
+total = 0
+for step in range(N):
+    print(step, "of", N, end="\r")
+
+    x1, x2, y = generator.val[step]
+
+    counts += np.sum(y)
+
+    total += 32
+
+    y_pred = model.predict([x1, x2])
+
+    y_preds.append(y_pred)
+    y_trues.append(y)
+
+y_preds = np.vstack(y_preds)
+y_trues = np.vstack(y_trues)
 
 
 # calculate scores
-ns_auc = roc_auc_score(y_val, ns_probs)
-lr_auc = roc_auc_score(y_val, lr_probs)
+ns_auc = roc_auc_score(y_trues, [0.5]*y_preds.shape[0])
+lr_auc = roc_auc_score(y_trues, y_preds)
+
+print("AUC:", lr_auc)
+
+print("positives", counts)
+print("Total:", total)
+
+print(y_trues.shape, y_preds.shape)
+#
 
 # calculate roc curves
-ns_fpr, ns_tpr, _ = roc_curve(y_val, ns_probs)
-lr_fpr, lr_tpr, _ = roc_curve(y_val, lr_probs)
+ns_fpr, ns_tpr, _ = roc_curve(y_trues, [0.5]*y_preds.shape[0])
+lr_fpr, lr_tpr, _ = roc_curve(y_trues, y_preds)
 
 # plot the roc curve for the model
 plt.figure(figsize=(5,5), dpi= 200)
 plt.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
 plt.plot(lr_fpr, lr_tpr, marker='.', label='Logistic')
-
+#
 # axis labels
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-
+#
 # show the plot
 plt.title('Logistic: ROC AUC=%.3f' % (lr_auc), fontweight='bold')
 plt.xlim([0,1])
 plt.ylim([0,1])
 plt.grid()
-plt.show()
 
+plt.savefig("./AUC_curve.png")
 
+plt.close()
 
+#plt.show()
+#
+#
+#
